148d33522e05d2723e730ca6859299b8d4300538
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-13 17:21:39 +0530

    Update Summary.js

--- src/Summary.js
+++ src/Summary.js
@@ -1,39 +1,44 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import levenshtein from "levenshtein";
+import "./Summary.css";
+import { CornerButton } from "./CornerButton";
 
 export const Summary = React.memo(
   function Summary(props) {
+    const [position, setPosition] = useState("top-right");
     const cards = Object.values(props.cards);
+    console.time('calc-distances');
+    const distances = useMemo(() => {
+      const distanceCalcs = { max: 0, min: 100000 };
+      cards.forEach(currentCard => {
+        cards.forEach(compareCard => {
+          if (compareCard === currentCard) {
+            return;
+          }
+          const distance = levenshtein(currentCard.label, compareCard.label);
 
-    const distances = { max: 0, min: 100000 };
-    cards.forEach(currentCard => {
-      cards.forEach(compareCard => {
-        if (compareCard === currentCard) {
-          return;
-        }
-        const distance = levenshtein(currentCard.label, compareCard.label);
-
-        distances.max = Math.max(distances.max, distance);
-        distances.min = Math.min(distances.min, distance);
+          distanceCalcs.max = Math.max(distanceCalcs.max, distance);
+          distanceCalcs.min = Math.min(distanceCalcs.min, distance);
+        });
       });
-    });
+      return distanceCalcs;
+    }, [Object.keys(cards).length]);
+    console.timeEnd('calc-distances')
 
     return (
-      <div
-        style={{
-          position: "absolute",
-          right: 20,
-          top: 20,
-          backgroundColor: "#fafafa",
-          padding: "10px",
-          border: "3px solid #333"
-        }}
-      >
+      <div className={`Summary Summary-${position}`}>
         <div>You have {Object.keys(props.cards).length} cards!</div>
         <div>Max difference in labels: {distances.max}</div>
         <div>Min difference in labels: {distances.min}</div>
+
+        <CornerButton setPosition={setPosition} corner="top-right" position={position} />
+        <CornerButton setPosition={setPosition} corner="top-left" position={position} />
+        <CornerButton setPosition={setPosition} corner="bottom-left" position={position} />
+        <CornerButton setPosition={setPosition} corner="bottom-right" position={position} />
       </div>
     );
-  }, 
-  (prevProps, newProps) => Object.values(prevProps.cards).length === Object.values(newProps.cards).length
+  },
+  (prevProps, newProps) =>
+    Object.values(prevProps.cards).length ===
+    Object.values(newProps.cards).length
 );
